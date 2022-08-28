c99be2a74c029cae05c0483362f88bfbd9e68b73
MikeWalder <mike_walder@hotmail.fr>
2022-08-28 23:17:26 +0200

    Modifs mineures - Assimilation des intercepteurs Axios

--- src/components/AdminHeader.vue
+++ src/components/AdminHeader.vue
@@ -1,12 +1,33 @@
 <template>
     <div class="admin_header">
         Header admin
+        
+            <button  @click="disconnectAccount()" class="btn_disconnect">Disconnect</button>
+        
+        <span v-if="validateDisconnection">Redirection...</span>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'HeaderAdmin'
+    name: 'HeaderAdmin',
+    data() {
+        return {
+            validateDisconnection: null,
+        }
+    },
+    methods: {
+        disconnectAccount() {
+            if(localStorage.getItem('token')) {
+                localStorage.removeItem('token')
+                this.validateDisconnection = true
+                setTimeout(() => {
+                    this.$router.push('/')
+                }, 800)
+                
+            }
+        }
+    }
 }
 </script>
 
@@ -17,5 +38,14 @@ export default {
         font-size: 22px;
         padding: 20px;
         background: linear-gradient(lightskyblue 0%, lightgreen 80%);
+    }
+    button {
+        display: inline-block;
+        
+    }
+    .btn_disconnect {
+        background-color: #dc3545;
+        color: white;
+        padding: 10px; 
     }
 </style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <h3>Connexion</h3>
-
+        <div class="text-center">
+            <h2>Connexion</h2>
+        </div>
+        
         <form @submit.prevent="submission">
             <div class="form_group">
                 <label for="input_pseudo">Pseudo : </label>
@@ -39,26 +41,38 @@ export default {
 </script>
 
 <style>
+    .text-center {
+        text-align: center;
+        font-weight: bold;
+        font-size: 32px;
+    }
     form {
         margin: 0 auto;
         margin-top: 45px;
         padding: 20px;
-        border: 2px solid black;
-        max-width: 50vw;
+        /* border: 2px solid black; */
+        max-width: 30vw;
     }
     form .form_group {
         padding: 10px;
     }
     form .form_group label {
-        width: 30px;
+        display: inline-block;
+        width: 180px;
     }
     form .form_group input {
         padding: 5px 10px;
+        border-radius : 3px;
+        width: 200px;
+        box-shadow: 2px 2px 4px black;
     }
     form .btn_login {
-        margin-top: 30px;
+        margin-top: 80px;
         padding: 10px 20px;
-        background-color: lightgreen;
-        margin-left: 5vw;
+        background-color: #28a745;
+        color: white;
+        font-size: 18px;
+        border-radius: 5px;
+        box-shadow: 5px 5px 10px black;
     }
 </style>
